9f56c6a1759f96c88f0193ac8357eb482e813ff7
Jeffrey H. Johnson <trnsz@pobox.com>
2021-03-19 03:05:50 -0400

    Merge Groundworkstech/dynamips-gdb-mod GDB support

--- common/utils.c
+++ common/utils.c
@@ -32,7 +32,7 @@
 
 #include "utils.h"
 
-extern FILE *log_file;
+//extern FILE *log_file;
 
 /* Add an element to a list */
 m_list_t *m_list_add(m_list_t **head,void *data)
@@ -279,7 +279,7 @@ void m_log(char *module,char *fmt,...)
    va_list ap;
 
    va_start(ap,fmt);
-   m_flog(log_file,module,fmt,ap);
+//   m_flog(log_file,module,fmt,ap);
    va_end(ap);
 }
 
